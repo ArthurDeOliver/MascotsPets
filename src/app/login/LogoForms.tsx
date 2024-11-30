1b2553ec69a1be06bb43660546c5e6ffd4861989
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-11-30 20:39:03 -0300

    last commit

--- src/app/login/LogoForms.tsx
+++ src/app/login/LogoForms.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/images/Logo.png";
 
 export function LogoForms() {
   return (
-    <div className="w-2/3">
+    <div className="w-2/3 flex justify-center">
       <Image alt="logo" src={logo.src} />
     </div>
   );
