4606d49b7f97005ff440b59dd361d94a836383ec
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-11-30 20:52:49 -0300

    aside

--- src/app/exames/AsideNavItem.tsx
+++ src/app/exames/AsideNavItem.tsx
@@ -20,13 +20,15 @@ export function AsideNavItem({
   return (
     <button
       onClick={onClick}
-      className={`${isAcess ? "p-4 rounded-full flex items-center gap-2 hover:bg-exames-color-800 hover:text-white transition-all bg-mascots-secundary-50" : "p-4 rounded-full flex items-center gap-2 hover:bg-exames-color-800 transition-all text-white active:bg-exames-color-500 active:text-black"} `}
+      className={`${isAcess ? "p-4 text-nowrap rounded-full flex items-center gap-2 hover:bg-exames-color-800 hover:text-white transition-all bg-mascots-secundary-50" : "p-4 rounded-full flex items-center gap-2 hover:bg-exames-color-800 transition-all text-white active:bg-exames-color-500 active:text-black"} `}
     >
       {icon}
       <div
         className={`${
-          !hover ? "opacity-0 max-w-0" : "opacity-100 max-w-full"
-        } text-base font-medium transition-all duration-300 overflow-hidden`}
+          !hover
+            ? "opacity-0 text-nowrap max-w-0"
+            : "opacity-100 text-nowrap max-w-full"
+        } text-base font-medium transition-all duration-300 text-nowrap overflow-hidden `}
       >
         {text}
       </div>
